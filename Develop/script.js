7acd3e6a1ee47f43466405cde68edc79f74f2c03
Zayad <zayad.khan.1998@gmail.com>
2021-01-09 08:03:33 -0500

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// creating an array of characters
+var chars = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9 0 ! @ # $ % ^ & *";
+var res = chars.split(" ");
+// console.log(chars)
+
+function getRandom(chars){
+  return String.fromCharCode(Math.floor(Math.random()))
+}
+console.log(getRandomchars)
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
